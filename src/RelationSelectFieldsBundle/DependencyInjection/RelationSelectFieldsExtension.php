e03053263a6a9094fbafc15174b174c4f84cce63
dominik <ds@nambu.ch>
2021-06-18 15:02:17 +0200

    removed config

--- src/RelationSelectFieldsBundle/DependencyInjection/RelationSelectFieldsExtension.php
+++ src/RelationSelectFieldsBundle/DependencyInjection/RelationSelectFieldsExtension.php
@@ -15,9 +15,6 @@ class RelationSelectFieldsExtension extends Extension {
      * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container) {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
